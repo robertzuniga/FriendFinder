87939b13734d2da1dde86afe8e89ce0bed715cee
Robert Zuniga <github@mastertheday.com>
2019-08-08 04:47:13 -0400

    getting files updated

--- server.js
+++ server.js
@@ -1,1 +1,89 @@
-// Robert Zuniga
+// Robert Zuniga                      server.js
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3030;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Friends Data
+var friends = [
+    {
+        routeName: "robertzuniga",
+        name: "robert",
+        photo: "https://robertzuniga.github.io/Bootstrap-Portfolio/assets/images/Robert-Zuniga.jpg",
+        scores: [
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        routeName: "mayazuniga",
+        name: "maya",
+        photo: "https://robertzuniga.github.io/Bootstrap-Portfolio/assets/images/technics-q-c-640-480-1.jpg",
+        scores: [
+            5,
+            1,
+            1
+        ]
+    }
+
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+// Displays all characters
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
+
+// Displays a single character, or returns false
+app.get("/api/friends/:friend", function(req, res) {
+  var chosen = req.params.friend;
+
+  console.log(chosen);
+  for (var i = 0; i < friends.length; i++) {
+    if (chosen === friends[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+
+// Create New Characters - takes in JSON input
+app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriend = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newFriend
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    characters.push(newFriend);
+  
+    res.json(newFriend);
+  });
+  
