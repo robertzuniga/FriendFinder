cceffde254a0e3eb8e94ab98da8df4a0e0702300
Robert Zuniga <github@mastertheday.com>
2019-08-08 01:42:14 -0400

    friends.js loaded with data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,38 @@
-//Robert Zuniga - friends.js
+//Robert Zuniga - friends.js 
+//Data 
+
+var myFriends = [
+    {
+        name: "Robert",
+        photo: "https://robertzuniga.github.io/Bootstrap-Portfolio/assets/images/Robert-Zuniga.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Maya",
+        photo: "https://robertzuniga.github.io/Bootstrap-Portfolio/assets/images/technics-q-c-640-480-1.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+
+];
