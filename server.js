91cab4348b8a990317b9e0e800d1ed9f6d8a100b
Robert Zuniga <github@mastertheday.com>
2019-08-18 03:31:05 -0400

    survey.html post correctly

--- server.js
+++ server.js
@@ -9,23 +9,14 @@ var path = require("path");
 var app = express();
 var PORT = 3030 || process.env.PORT;
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-
-
-
-
-
-  
-// Starts the server to begin listening
+  // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
